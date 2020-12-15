9c28e7b18833124aac5a528836b359abbc4c1ae8
mancubus <aby@daby.com>
2020-12-15 23:29:33 +1100

    add encoding

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def run():
             metric["ts"] = datetime.utcnow().isoformat(sep=' ', timespec='milliseconds')
             print(json.dumps(metric))
             # client.write_points([metric])
-            producer.send(TOPIC, bytes(json.dumps(metric)))
+            producer.send(TOPIC, bytes(json.dumps(metric),encoding='utf8'))
 
 
 if __name__ == "__main__":
